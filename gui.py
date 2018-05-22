999231d42f090efe3594eb16be3e9263f19d552a
Aness <menaa.aness2@gmail.com>
2018-05-22 19:53:53 +0200

    beginning interface

---
+++ gui.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from tkinter import * 
+import tkinter as tk
+
+def test():
+	print('test')
+
+fenetre = Tk()
+
+label = Label(fenetre, text="mov r23 #3", bg="white", width=25, anchor='w')
+label.pack()
+label = Label(fenetre,text="-> "+"prn r23", bg="white", width=25, anchor='w')
+label.pack()
+label = Label(fenetre, text="add r22 #3 #2", bg="white", width=25, anchor='w')
+label.pack()
+
+
+b = Button(fenetre, text="Pas-à-pas", command=test).pack(padx=10, pady=10, side = LEFT)
+b = Button(fenetre, text="Sans interruption", command=test).pack(padx=10, pady=10, side = RIGHT)
+
+
+fenetre.mainloop()
