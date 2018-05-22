714a017ba7b81cb4da8b4dc83f4f3f8fe2229463
Aness <menaa.aness2@gmail.com>
2018-05-22 19:58:41 +0200

    beginning interface

--- interface/gui.py
+++ interface/gui.py
@@ -16,23 +16,8 @@ label.pack()
 label = Label(fenetre, text="add r22 #3 #2", bg="white", width=25, anchor='w')
 label.pack()
 
-# b = Button(fenetre, text ='test').pack(side=LEFT, padx=5, pady=5)
-# b.bind('test','<Button-1>', test)
-# b.pack()
-
-# canvas = Canvas(fenetre, width=70, height=20)
-# txt = canvas.create_text(5, 10, text="Pas-à-pas", fill="black", anchor='w')
-# canvas.focus_set()
-# canvas.bind("<Button-1>", test)
-# canvas.pack()
-
-# Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
-# Frame1.pack(side=LEFT, padx=30, pady=30)
-# b = Label(Frame1, text="Frame 1").pack(padx=10, pady=10)
-# b.bind("<Button-1", test)
 
 b = Button(fenetre, text="Pas-à-pas", command=test).pack(padx=10, pady=10, side = LEFT)
-
 b = Button(fenetre, text="Sans interruption", command=test).pack(padx=10, pady=10, side = RIGHT)
 
 
