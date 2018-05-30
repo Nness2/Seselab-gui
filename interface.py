b75e3bbcd15d4fda29ce0bc5caee36515a41cd8b
Aness <menaa.aness2@gmail.com>
2018-05-30 05:52:29 +0200

    clean

--- interface.py
+++ interface.py
@@ -4,24 +4,24 @@ from tkinter import *
 from compiler import Compiler
 import linecache
 
-# def getLines(file):
-# 	instrs = []
-# 	with open(file) as instr :
-# 		for line in instr :
-# 			if line != '\n':
-# 				instrs.append(line.strip())
-# 	instr.close()
-# 	instrs.append('end')
-# 	instrs.append('')
-# 	return instrs
 
-def getInfo (l,i):
-	file = l[i][1][0]
-	line =l[i][1][1]-1
+def getInfo (program,i):
+	file = program[i][1][0]
+	line =program[i][1][1]-1
 	code = linecache.getline(file, line)
 	code = code.replace('\t','')
 	code = code.replace('\n','')
 	return [code,file,line]
+
+def getInfos(program):
+	i = 0
+	instrs = []
+	for instr in program:
+		instrs.append(getInfo(program,i))
+		i += 1
+	instrs.append("end")
+	instrs.append("")
+	return instrs
 
 class Interface:
 
@@ -31,8 +31,7 @@ class Interface:
 		self._root.title('Seselab') 
 		self._root.geometry("680x240")
 		self._code = code
-		self._infos = self.getInfos(self._code)
-		# self._lines = getLines(sys.argv[1])
+		self._infos = getInfos(self._code)
 
 		self._t = Text(self._root,height=6, width=45)
 		self._t.grid(padx=10, pady=10, column = 2, row = 1)
@@ -46,22 +45,6 @@ class Interface:
 
 		self._step = Button(self._root, text="Step by step", command=self.stepByStep).grid(column=1, row=4, sticky='ew')
 		self._quit = Button(self._root, text="Close", command=self._root.quit).grid(column=1, row=7, sticky='ew', pady = 4)
-		# self._rstr = Button(self._root, text="Restart", width=5, command=self.nStep).grid(column=1, row=6, sticky='ew')
-		# self._nStep = Button(self._root, text="Jump", width=5, command=self.nStep).grid(column=1, row=5, sticky='ew')
-		# self._value = StringVar() 
-		# self._value.set('Step')
-		# self._entree = Entry(self._root, text=self._value, width=5)
-		# self._entree.grid(column=2, row=5, sticky='ew')
-
-	def getInfos(self, program):
-		i = 0
-		instrs = []
-		for instr in program:
-			instrs.append(getInfo(program,i))
-			i += 1
-		instrs.append("end")
-		instrs.append("")
-		return instrs
 
 	def change(self, i):
 		self._t.config(state='normal')
@@ -78,18 +61,7 @@ class Interface:
 			self.change(self._i)
 			self._i += 1
 
-	# def restart(self):
-	# 	self._i = 0
-	# 	self.change(self._i-1)
 
-	# def nStep(self):
-	# 	i = 0
-	# 	n = self._entree.get()
-	# 	while i < int(n[0]):	#gérer les erreurs
-	# 		if self._lines[self._i] != 'end':
-	# 			self.change(self._i)
-	# 			self._i += 1
-	# 		i += 1	
 
 
 program = Compiler().compile(sys.argv[1])
