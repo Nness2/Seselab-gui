4276a9c21f1f6b03a988f4e33fa666bf3fc88167
Aness <menaa.aness2@gmail.com>
2018-05-30 06:27:00 +0200

    success passge to label

--- interface.py
+++ interface.py
@@ -11,7 +11,7 @@ def getInfo (program,i):
 	code = linecache.getline(file, line)
 	code = code.replace('\t','')
 	code = code.replace('\n','')
-	return [code,file,line]
+	return [code,file,line,program[i][0]]
 
 def getInfos(program):
 	i = 0
@@ -29,22 +29,21 @@ class Interface:
 		self._i = 0
 		self._root = Tk()
 		self._root.title('Seselab') 
-		self._root.geometry("680x240")
-		self._code = code
-		self._infos = getInfos(self._code)
-		self._lab1 = Label(self._root, text=' ', bg='white', width=80)
-		self._lab2 = Label(self._root, text=str(self._infos[1])+"\t"+str(code[0][0]), bg='white', width=80)
-		self._lab3 = Label(self._root, text=str(self._infos[2])+"\t"+str(code[1][0]), bg='white', width=80)
-		self._lab1.grid(padx=10, pady=10, column = 1, row = 1)
-		self._lab2.grid(padx=10, pady=10, column = 1, row = 2)
-		self._lab3.grid(padx=10, pady=10, column = 1, row = 3)
+		self._root.geometry("480x140")
+		self._infos = getInfos(code)
+		self._lab1 = Label(self._root, text='', bg='white', width=50, anchor='w')
+		self._lab2 = Label(self._root, text=str(self._infos[1]), bg='yellow', width=50, anchor='w')
+		self._lab3 = Label(self._root, text=str(self._infos[2]), bg='white', width=50, anchor='w')
+		self._lab1.grid(column = 1, row = 1, sticky='w')
+		self._lab2.grid(column = 1, row = 2, sticky='w')
+		self._lab3.grid(column = 1, row = 3, sticky='w')
 		self._step = Button(self._root, text="Step by step", command=self.stepByStep).grid(column=1, row=4, sticky='ew')
 		self._quit = Button(self._root, text="Close", command=self._root.quit).grid(column=1, row=7, sticky='ew', pady = 4)
 
 	def change(self, i):
-		self._lab1.config(text=str(self._infos[self._i+1])+'\t'+str(self._code[i][0]))
-		self._lab2.config(text=str(self._infos[self._i+2])+'\t'+str(self._code[i+1][0]))
-		self._lab3.config(text=str(self._infos[self._i+3])+'\t'+str(self._code[i+2][0]))
+		self._lab1.config(text=str(self._infos[self._i+1]))
+		self._lab2.config(text=str(self._infos[self._i+2]))
+		self._lab3.config(text=str(self._infos[self._i+3]))
 
 	def stepByStep(self):
 		if self._infos[self._i] != 'end':
